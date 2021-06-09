bc003e82a1b80f171409228a307fab18a11aace4
Danny101M <82120349+Danny101D@users.noreply.github.com>
2021-06-09 22:35:02 +0000

    n

---
+++ ProyectoMaven/src/upeu/App.java
@@ -0,0 +1,13 @@
+paquete  pe.edu.upeu ;
+
+/ **
+ * ¡Hola Mundo!
+ *
+ * /
+ Aplicación de clase  pública {
+    public  static  void  main ( String [] args ) {
+
+        Sistema . fuera . println ( " ¡Hola mundo! " );
+        
+    }
+}
---
+++ ProyectoVentasG3/src/app/AppTest.java
@@ -0,0 +1,38 @@
+paquete  pe.edu.upeu.app ;
+
+importar  junit.framework.Test ;
+import  junit.framework.TestCase ;
+import  junit.framework.TestSuite ;
+
+/ **
+ * Prueba unitaria para una aplicación simple.
+ * /
+público  de clase  AppTest 
+    extiende  TestCase
+{
+    / **
+     * Crea el caso de prueba
+     *
+     * @param testName nombre del caso de prueba
+     * /
+    pública  AppTest ( Cadena  testName )
+    {
+        super (nombre de prueba);
+    }
+
+    / **
+     * @return el conjunto de pruebas que se están probando
+     * /
+     Conjunto de pruebas estáticas  públicas () 
+    {
+        return  new  TestSuite ( clase AppTest . );
+    }
+
+    / **
+     * Prueba rigurosa :-)
+     * /
+     testApp público vacío  ()
+    {
+        asertTrue ( verdadero );
+    }
+}
